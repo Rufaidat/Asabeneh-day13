76ff0ed665e0eee644c467f536249b583ddbb1bf
Rufaidat <alsiddiqrufaidat@gmail.com>
2022-07-06 18:33:55 +0100

    console() methods

---
+++ main.js
@@ -0,0 +1,41 @@
+// level 1
+// que 1
+const countries = [
+  ["Finland", "Helsinki"],
+  ["Sweden", "Stockholm"],
+  ["Norway", "Oslo"],
+];
+console.table(countries);
+
+// que 2
+console.log("\n");
+const user = {
+  name: "Asabeneh",
+  title: "Programmer",
+  country: "Finland",
+  city: "Helsinki",
+  age: 250,
+};
+console.table(user);
+
+// que 3
+console.log("\n");
+console.group(countries);
+console.group(user);
+
+//level 2
+// que 1
+console.log("\n");
+let solut
+console.log('the # is ' + i)
+console.assert(i % 2 === 0, { number: i, errorMessage: errorMessage })
+console.assert(10 > 2 * 10, {answer: });
+// que 2
+console.log("\n");
+console.warn("this is a warning");
+
+// que 3
+console.log("\n");
+console.log("this is an error");
+
+// level 3
