177694e07f5f037266488815cea353a8a4a792f5
Rufaidat <alsiddiqrufaidat@gmail.com>
2022-07-07 08:58:16 +0100

    level 3

--- main.js
+++ main.js
@@ -26,10 +26,7 @@ console.group(user);
 //level 2
 // que 1
 console.log("\n");
-let solut
-console.log('the # is ' + i)
-console.assert(i % 2 === 0, { number: i, errorMessage: errorMessage })
-console.assert(10 > 2 * 10, {answer: });
+console.assert(10 > 2 * 10, "answer");
 // que 2
 console.log("\n");
 console.warn("this is a warning");
@@ -39,3 +36,29 @@ console.log("\n");
 console.log("this is an error");
 
 // level 3
+console.log("\n");
+console.time("Regular for loop");
+for (let i = 0; i < countries.length; i++) {
+  console.log(countries[i][0], countries[i][1]);
+}
+console.timeEnd("Regular for loop");
+
+console.time("for of loop");
+for (const [name, city] of countries) {
+  console.log(name, city);
+}
+console.timeEnd("for of loop");
+
+console.time("forEach loop");
+countries.forEach(([name, city]) => {
+  console.log(name, city);
+});
+console.timeEnd("forEach loop");
+
+console.time("while loop");
+let i = 0;
+while (i < countries.length) {
+  console.log(countries[i][0], countries[i][1]);
+  i++;
+}
+console.timeEnd("while loop");
